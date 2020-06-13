7ab453c776c0974e50f4ece241abaf10dd5f23d9
Dogac <ndakyildiz@gmail.com>
2020-06-14 01:01:33 +0300

    Add Card.

---
+++ TCGSolution/TCGCore/Card.cs
@@ -0,0 +1,38 @@
+﻿using System;
+
+namespace TradingCardGameCore
+{
+    public class Card : IComparable<Card>
+    {
+        public int Value { get; }
+
+        public Card(int value)
+        {
+            this.Value = value;
+        }
+
+        public static implicit operator Card(int value) => new Card(value);
+
+        public override bool Equals(object obj)
+        {
+            var card = obj as Card;
+
+            if (card == null)
+                return false;
+
+            return this.Value == card.Value;
+        }
+
+        public override int GetHashCode()
+        {
+            return this.Value;
+        }
+
+        public int CompareTo(Card other)
+        {
+            if (ReferenceEquals(this, other)) return 0;
+            if (ReferenceEquals(null, other)) return 1;
+            return Value.CompareTo(other.Value);
+        }
+    }
+}
--- TCGSolution/TCGCore/Program.cs
+++ TCGSolution/TCGCore/Program.cs
@@ -1,10 +1,12 @@
-﻿namespace TradingCardGameCore
+﻿using System;
+
+namespace TCGCore
 {
     class Program
     {
         static void Main(string[] args)
         {
-
+            Console.WriteLine("Hello World!");
         }
     }
 }
